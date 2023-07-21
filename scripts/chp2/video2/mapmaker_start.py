07037c37ab60ff1df345cf3a11acb6a407f1dd12
tahmeedh <60047767+tahmeedh@users.noreply.github.com>
2023-07-21 09:46:09 -0700

    write unit tests

--- scripts/chp2/video2/mapmaker_start.py
+++ scripts/chp2/video2/mapmaker_start.py
@@ -1,2 +1,8 @@
 class Point():
-    pass
+    def __init__(self, name, lat, longit):
+        self._name = name
+        self._lat = lat
+        self._long = longit
+    
+    def get_lat_long(self):
+        return(self._lat , self._long)
