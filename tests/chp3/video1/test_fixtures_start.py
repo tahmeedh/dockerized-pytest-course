a5e1db4180cc33b93c739a73ef2d1fac1589d1de
tahmeedh <60047767+tahmeedh@users.noreply.github.com>
2023-07-23 02:22:39 -0700

    fixtures

--- tests/chp3/video1/test_fixtures_start.py
+++ tests/chp3/video1/test_fixtures_start.py
@@ -1,15 +1,12 @@
 import pytest
 from scripts import data_processor, map_population_update
 
+@pytest.fixture(scope="function")
+def map_data_location():
+    yield 'tests/resources/cities/clean_map.csv'
 
-def test_data_population_update():
-    """
-    Happy Path test
-    """
-    map_data_location = 'tests/resources/cities/clean_map.csv'
-    data = data_processor.csv_reader(map_data_location)
-    data_to_transform = map_population_update.MapData(data, False)
-
+@pytest.fixture(scope="function")
+def prep_transform_data(map_data_location):
     population_dict = {
             'Andorra': 77142,
             'Argentina': 44780677,
@@ -22,38 +19,32 @@ def test_data_population_update():
             'Senegal': 16296364,
             'United States': 329064917
     }
+    data = data_processor.csv_reader(map_data_location)
+    data_to_transform = map_population_update.MapData(data, False)
+    yield data_to_transform, population_dict
 
-    data_to_transform.add_population(population_dict)   # transform object in place
+
+def test_data_population_update(prep_transform_data):
+    """
+    Happy Path test
+    """
+    data_to_transform, population_dict = prep_transform_data
+    data_to_transform.add_population(population_dict)
 
     for row in data_to_transform.get_data():
         assert 'Population' in row
         assert 'Updated' in row
 
 
-def test_data_population_no_update():
+def test_data_population_no_update(prep_transform_data):
     """
     Sad Path test: We don't want the data transformed twice
 
     """
 
-    map_data_location = 'tests/resources/cities/clean_map.csv'
-    data = data_processor.csv_reader(map_data_location)
-    data_to_transform = map_population_update.MapData(data, False)
+    data_to_transform, population_dict = prep_transform_data
+    data_to_transform.add_population(population_dict)
 
-    population_dict = {
-            'Andorra': 77142,
-            'Argentina': 44780677,
-            'Cape Verde': 546388,
-            'Germany': 83670653,
-            'Greece': 10473455,
-            'India': 1366417754,
-            'Japan': 128860301,
-            'Morocco': 36471769,
-            'Senegal': 16296364,
-            'United States': 329064917
-    }
-
-    data_to_transform.add_population(population_dict)   # transform object in place
     # Try calling the function again
     with pytest.raises(Exception) as e:
         data_to_transform.add_population(population_dict)
