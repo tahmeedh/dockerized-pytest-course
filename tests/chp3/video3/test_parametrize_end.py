5d899f5690ca4b134ca43472b98798966287537e
tahmeedh <60047767+tahmeedh@users.noreply.github.com>
2023-09-20 01:37:01 -0700

    test

--- tests/chp3/video3/test_parametrize_end.py
+++ tests/chp3/video3/test_parametrize_end.py
@@ -24,6 +24,23 @@ def process_data(city_list_location):
 
     yield _specify_type
 
+@pytest.fixture(scope="module")
+def process_data(city_list_location):
+    files = os.listdir(city_list_location)
+
+    def _specify_type(file_name_or_type):
+        for f in files:
+            if file_name_or_type in f:
+                if file_name_or_type != '.json':
+                    data = data_processor.csv_reader(city_list_location + f)
+                else:
+                    data = data_processor.json_reader(city_list_location + f)
+        return data
+
+    yield _specify_type
+
+
+
 
 @pytest.mark.parametrize("country,stat,expected", [
     ('Andorra', 'Mean', 1641.42),
